23ccc25c9e485fd80b95f810b1ba2f3a81c5b02d
Usman Ali <114804406+Usman-tpr@users.noreply.github.com>
2023-09-30 23:31:38 +0500

    contact-backend

--- client/src/Contact Us Page/ContactUsPage.jsx
+++ client/src/Contact Us Page/ContactUsPage.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 import '../Contact Us Page/ContactUsPage.css'
 import FooterPage from '../Footer Page/FooterPage'
-
+import {toast} from 'react-toastify'
 export default function ContactUsPage() {
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [phone,setPhone] = useState('');
+    const [message,setMessage] = useState('');
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+        try {
+            const response = await fetch('http://localhost:5000/api/user/post-message',{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({
+                    name:name,
+                    email:email,
+                    phone:phone,
+                    message:message
+                })
+            })
+            const data = await response.json();
+          
+              toast.success("Message sent successfully")
+          
+        } catch (error) {
+               toast.error(error.message);
+        }
+    }
     return (
         <>
             <Navbar />
@@ -17,11 +45,11 @@ export default function ContactUsPage() {
                         <p className='contact_address'><strong>Email:</strong> <Link target='_blank' to="mailto:waqmeh456@gmail.com">dyonsrkpk@gmail.com</Link></p>
                     </div>
                     <div className="col-lg-6 col-sm-12">
-                        <form action="" className='d-flex flex-column '  >
-                            <input type="text" placeholder='Name' name='name' className='custom-contact-input mt-5' />
-                            <input type="email" placeholder='Email' name='email' className='custom-contact-input mt-4' />
-                            <input type="text" placeholder='Phone' name='phone' className='custom-contact-input mt-4' />
-                            <textarea type="text" placeholder='Message' name='message' className='custom-contact-input mt-4' />
+                        <form action="" className='d-flex flex-column '  onSubmit={handleSubmit}>
+                            <input type="text" placeholder='Name' name='name' className='custom-contact-input mt-5' onChange={e=>{setName(e.target.value)}}/>
+                            <input type="email" placeholder='Email' name='email' className='custom-contact-input mt-4' onChange={e=>{setEmail(e.target.value)}} />
+                            <input type="text" placeholder='Phone' name='phone' className='custom-contact-input mt-4' onChange={e=>{setPhone(e.target.value)}}/>
+                            <textarea type="text" placeholder='Message' name='message' className='custom-contact-input mt-4' onChange={e=>{setMessage(e.target.value)}}/>
                             <button className='button p-1 bg-custom text-white mt-3 rounded-3'  type='submit'>Send Message</button>
                         </form>
                     </div>
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/userModel')
-
+const Contact = require('../models/contactModel')
 //register controller
 const registerController = async(req,res)=>{
     const { name , email ,password } = req.body;
@@ -61,4 +61,30 @@ const registerController = async(req,res)=>{
             }
         
         }
- module.exports = {registerController , loginController}
+
+        const contactController = async(req,res)=>{
+                  
+                  try {
+                    const { name,email,phone,message } = req.body;
+                    const newContact = await Contact({
+                     name,
+                     email,
+                     phone,
+                     message
+                    });
+ 
+                    await newContact.save();
+                    return res.status(200).send({message:"Message sent successfully"});
+                  } catch (error) {
+                    return res.status(500).send({message:error.message});
+                  }
+        }
+        const getContactController = async(req,res)=>{
+            try {
+                const contacts = await Contact.find();
+                return res.status(200).send(contacts);
+            } catch (error) {
+                return res.status(500).send({message:error.message});
+            }
+        }
+ module.exports = {registerController , loginController ,getContactController, contactController}
